591a4a327b81265604e4f49515426794dc2c22ba
amydady <215952449@qq.com>
2018-09-09 19:05:08 +0800

    NONE

--- src/main/java/com/littlecat/cbb/common/Consts.java
+++ src/main/java/com/littlecat/cbb/common/Consts.java
@@ -2,8 +2,8 @@ package com.littlecat.cbb.common;
 
 public final class Consts
 {
-	public static final String NONE_ERROR_CODE = "0";
-	public static final String UNKNOW_ERROR_CODE = "-1";
+	public static final String NONE_ERROR_CODE = "NONE_ERROR";
+	public static final String UNKNOW_ERROR_CODE = "UNKNOW_ERROR";
 
 	private Consts()
 	{
