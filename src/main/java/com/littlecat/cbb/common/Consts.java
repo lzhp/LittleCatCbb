0821b73e56bea33c5e58ea038d17e3bb7044326c
amydady <215952449@qq.com>
2018-09-11 23:11:51 +0800

    NONE

--- src/main/java/com/littlecat/cbb/common/Consts.java
+++ src/main/java/com/littlecat/cbb/common/Consts.java
@@ -5,7 +5,7 @@ public final class Consts
 	public static final String ERROR_CODE_NONE = "NONE_ERROR";
 	public static final String ERROR_CODE_UNKNOW = "UNKNOW_ERROR";
 	public static final String ERROR_CODE_FROM_JSON_ERROR = "FROM_JSON_ERROR";
-	public static final String ERROR_CODE_TO_JSON_ERROR = "FROM_TO_ERROR";
+	public static final String ERROR_CODE_TO_JSON_ERROR = "TO_JSON_ERROR";
 	
 	
 
