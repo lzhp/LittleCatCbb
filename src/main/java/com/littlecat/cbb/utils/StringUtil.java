06e34c9435303bb38daf2fc928fb1df83c1ce564
amydady <215952449@qq.com>
2018-09-23 17:00:59 +0800

    NONE

--- src/main/java/com/littlecat/cbb/utils/StringUtil.java
+++ src/main/java/com/littlecat/cbb/utils/StringUtil.java
@@ -1,20 +1,40 @@
 package com.littlecat.cbb.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 public final class StringUtil
 {
+	private static final String DEFAULT_SPLIT_CHAR = ",";
+
 	private StringUtil()
 	{
 	}
-	
+
 	public static boolean isEmpty(String str)
 	{
 		return StringUtils.isEmpty(str);
 	}
-	
+
 	public static boolean isNotEmpty(String str)
 	{
 		return !StringUtils.isEmpty(str);
+	}
+
+	public static List<String> split2List(String str)
+	{
+		return split2List(str,DEFAULT_SPLIT_CHAR);
+	}
+	
+	public static List<String> split2List(String str,String splitChar)
+	{
+		if(StringUtil.isEmpty(str) || splitChar == null)
+		{
+			return null;
+		}
+		
+		return Arrays.asList(StringUtils.split(str, splitChar));
 	}
 }
