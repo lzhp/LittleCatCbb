b7b272d704443250030ff42640805d105057157a
amydady <215952449@qq.com>
2018-09-29 20:44:53 +0800

    NONE

--- src/main/java/com/littlecat/cbb/utils/DateTimeUtil.java
+++ src/main/java/com/littlecat/cbb/utils/DateTimeUtil.java
@@ -47,17 +47,26 @@ public final class DateTimeUtil
 		return Integer.valueOf(new SimpleDateFormat("dd").format(new Date(dateValue)));
 	}
 
-	// public static void main(String[] args)
-	// {
-	// Date date = new Date();
-	//
-	// System.out.println(date.getTime());
-	// SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-	// System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
-	//
-	// System.out.println(getCurrentTimeForDisplay());
-	// System.out.println(getYear(date.getTime()));
-	// System.out.println(getMonth(date.getTime()));
-	// System.out.println(getDay(date.getTime()));
-	// }
+	public static int getDurationDays(long dateValueStart, long dateValueEnd)
+	{
+		long dayStart = dateValueStart - dateValueStart % (1000 * 3600 * 24);
+		long dayEnd = dateValueEnd - dateValueEnd % (1000 * 3600 * 24);
+		return (int) (dayEnd - dayStart) / (1000 * 3600 * 24);
+	}
+
+//	public static void main(String[] args)
+//	{
+//		 Date date = new Date();
+//		
+//		 System.out.println(date.getTime());
+//		 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
+//		 System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+//		
+//		 System.out.println(getCurrentTimeForDisplay());
+//		 System.out.println(getYear(date.getTime()));
+//		 System.out.println(getMonth(date.getTime()));
+//		 System.out.println(getDay(date.getTime()));
+//
+//		System.out.println(getDurationDays(new Date().getTime(), new Date().getTime() + 1000 * 3600 * 24 * 3 + 1000 * 3600 ));
+//	}
 }
