cbf96a2d6ce267fdf3e30c40f7ccd612de24d0cf
amydady <215952449@qq.com>
2018-10-02 09:07:45 +0800

    NONE

--- src/main/java/com/littlecat/cbb/common/Consts.java
+++ src/main/java/com/littlecat/cbb/common/Consts.java
@@ -6,7 +6,7 @@ public final class Consts
 	public static final String ERROR_CODE_UNKNOW = "UNKNOW_ERROR";
 	public static final String ERROR_CODE_FROM_JSON_ERROR = "FROM_JSON_ERROR";
 	public static final String ERROR_CODE_TO_JSON_ERROR = "TO_JSON_ERROR";
-	
+	public static final String ERROR_CODE_PARSE_DATE_ERROR = "PARSE_DATE_ERROR";
 	
 
 	public static final String COMMON_DB_RESULT_FIELDS_TOTALNUM = "TotalNum";
--- src/main/java/com/littlecat/cbb/utils/DateTimeUtil.java
+++ src/main/java/com/littlecat/cbb/utils/DateTimeUtil.java
@@ -4,9 +4,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.littlecat.cbb.common.Consts;
+import com.littlecat.cbb.exception.LittleCatException;
+
 public final class DateTimeUtil
 {
-	private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	public static final SimpleDateFormat defaultDateFormat = new SimpleDateFormat(DEFAULT_DATE_FORMAT);// 设置日期格式
 
 	private DateTimeUtil()
 	{
@@ -20,7 +24,7 @@ public final class DateTimeUtil
 
 	public static String getCurrentTimeForDisplay()
 	{
-		return getCurrentTimeForDisplay(new SimpleDateFormat(DEFAULT_DATE_FORMAT));
+		return getCurrentTimeForDisplay(defaultDateFormat);
 	}
 
 	public static String getCurrentTimeForDisplay(SimpleDateFormat df)
@@ -52,24 +56,63 @@ public final class DateTimeUtil
 	{
 		long dayStart = dateValueStart - dateValueStart % (1000 * 3600 * 24);
 		long dayEnd = dateValueEnd - dateValueEnd % (1000 * 3600 * 24);
-		return (int) (dayEnd - dayStart) / (1000 * 3600 * 24);
+		return (int) ((dayEnd - dayStart) / (1000 * 3600 * 24));
+	}
+
+	/**
+	 * 获取两个日期之间相隔的天数
+	 * 
+	 * @param dateStrStart
+	 *            开始日期，字符串：yyyy-MM-dd HH:mm:ss
+	 * @param dateStrEnd
+	 *            结束日期，字符串：yyyy-MM-dd HH:mm:ss
+	 * @return
+	 */
+	public static int getDurationDays(String dateStrStart, String dateStrEnd)
+	{
+		try
+		{
+			long dayStart = defaultDateFormat.parse(dateStrStart).getTime();
+			long dayEnd = defaultDateFormat.parse(dateStrEnd).getTime();
+
+			return getDurationDays(dayStart, dayEnd);
+		}
+		catch (ParseException e)
+		{
+			throw new LittleCatException(Consts.ERROR_CODE_PARSE_DATE_ERROR, "parse date from string error", e);
+		}
+	}
+
+	public static int getDurationDays(String dateStrStart)
+	{
+		try
+		{
+			return getDurationDays(defaultDateFormat.parse(dateStrStart).getTime(), new Date().getTime());
+		}
+		catch (ParseException e)
+		{
+			throw new LittleCatException(Consts.ERROR_CODE_PARSE_DATE_ERROR, "parse date from string error", e);
+		}
 	}
 
 	public static void main(String[] args) throws ParseException
 	{
-		 Date date = new Date();
-		
-		 System.out.println(date.getTime());
-		 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-		 System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
-		
-		 System.out.println(getCurrentTimeForDisplay());
-		 System.out.println(getYear(date.getTime()));
-		 System.out.println(getMonth(date.getTime()));
-		 System.out.println(getDay(date.getTime()));
+		Date date = new Date();
 
-		System.out.println(getDurationDays(new Date().getTime(), new Date().getTime() + 1000 * 3600 * 24 * 3 + 1000 * 3600 ));
+		System.out.println(date.getTime());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
+		System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+
+		System.out.println(getCurrentTimeForDisplay());
+		System.out.println(getYear(date.getTime()));
+		System.out.println(getMonth(date.getTime()));
+		System.out.println(getDay(date.getTime()));
+
+		System.out.println(getDurationDays(new Date().getTime(), new Date().getTime() + 1000 * 3600 * 24 * 3 + 1000 * 3600));
 		System.out.println(getCurrentTime());
 		System.out.println(df.format(df.parse("2018-09-30 16:48:09.0")));
+
+		System.out.println(getDurationDays("2017-01-30 16:48:09", "2018-10-2 16:48:09"));
+
 	}
 }
